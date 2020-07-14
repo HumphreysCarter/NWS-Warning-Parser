a71385caddfb9fc16d721c4a79eda7a329d6ae8f
Carter Humphreys <55723784+HumphreysCarter@users.noreply.github.com>
2020-07-14 19:47:33 -0400

    Timezone support, added missing implemented
    Implemented pytz for VTEC string parse, added find_nthIndex method, implemented datetime

--- VTEC Parser/VTEC.py
+++ VTEC Parser/VTEC.py
@@ -1,15 +1,22 @@
 """
 NWS Valid Time Event Code (VTEC) string parser
 Based on NWS Directive NWSI 10-1703: https://www.nws.noaa.gov/directives/sym/pd01017003curr.pdf
-
 07/14/2020
 
 --------------------------------------------------------------------------------
-
 Copyright (c) 2020, Carter J. Humphreys (chumphre@oswego.edu)
 All rights reserved.
-
 """
+
+from datetime import datetime
+import pytz
+
+def find_nthIndex(fullString, find, n):
+    start = fullString.find(find)
+    while start >= 0 and n > 1:
+        start = fullString.find(find, start+len(find))
+        n -= 1
+    return start
 
 def parseVTEC(vtec_string): 
     # Product Class
@@ -32,13 +39,17 @@ def parseVTEC(vtec_string):
     nnnn=int(nnnn)
 
     # Date/Time Group
+    utc = pytz.utc
     begin=vtec_string[find_nthIndex(vtec_string, '.', 6)+1:find_nthIndex(vtec_string, '-', 0)]
     if begin == '000000T0000Z':
         begin = None
     else:
         begin=datetime.strptime(begin, '%y%m%dT%H%MZ')
+        begin=utc.localize(begin)
     end=vtec_string[find_nthIndex(vtec_string, '-', 0)+1:find_nthIndex(vtec_string, '/', 2)]
     end=datetime.strptime(end, '%y%m%dT%H%MZ')
-
+    end=utc.localize(end)
+    
+    
     # Return data
     return {'VTEC':vtec_string, 'ProductClass':k, 'Actions':aaa, 'OfficeID':cccc, 'Phenomena':pp, 'Significance':s, 'ETN':nnnn, 'EventBeginning':begin, 'EventEnding':end}
